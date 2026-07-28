5f343989c3b078d4f2c253ee19b3167321c27488
Fenohasina <fenohasinaratsimanarisoa@gmail.com>
2026-07-28 15:55:55 +0300

    fix: import GeocodingModule in DeliveriesModule + export GeocodingService

--- backend/src/modules/deliveries/deliveries.module.ts
+++ backend/src/modules/deliveries/deliveries.module.ts
@@ -3,10 +3,11 @@ import { DeliveriesService } from './deliveries.service';
 import { DeliveriesController } from './deliveries.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { WebhooksModule } from '../webhooks/webhooks.module';
+import { GeocodingModule } from '../geocoding/geocoding.module';
 import { UsageGuard } from '../../common/guards/usage.guard';
 
 @Module({
-  imports: [NotificationsModule, WebhooksModule],
+  imports: [NotificationsModule, WebhooksModule, GeocodingModule],
   controllers: [DeliveriesController],
   providers: [DeliveriesService, UsageGuard],
   exports: [DeliveriesService],
